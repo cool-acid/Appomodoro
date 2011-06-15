e0e4157c766181383a7ffcc24dd422c027196d36
Cool Acid <coolacid217@gmail.com>
2011-06-15 16:32:52 -0500

    Funcionando el contador de 25 minutos, ya se muestra en los canvas

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python
+import os
 from google.appengine.ext import webapp
+from google.appengine.ext.webapp import template
 from google.appengine.ext.webapp.util import run_wsgi_app
 
 class MainPage(webapp.RequestHandler):
     def get(self):
-        self.response.headers['Content-Type'] = 'text/plain'
-        self.response.out.write('Hello, webapp World!')
+        #por lo mientras solo vamos a mostrar la pagina tal cual. Sin pre-processing.
+        path = os.path.join(os.path.dirname(__file__), 'pruebacanvas.html')
+        self.response.out.write(template.render(path, None))
 
 application = webapp.WSGIApplication([
     ('/', MainPage)
