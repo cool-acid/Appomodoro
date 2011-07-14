e86d4a22fa4d96879b49c711d5559fe85754d319
Cool Acid <coolacid217@gmail.com>
2011-07-14 01:46:54 -0500

    Segun yo ya quedo. Mañana lo implementamos

--- czz.py
+++ czz.py
@@ -1,3 +1,5 @@
+
+# -*- coding: utf-8 -*-
 import os
 import re
 from google.appengine.ext import webapp
@@ -6,25 +8,33 @@ from google.appengine.ext.webapp.util import run_wsgi_app
 class Czz(webapp.RequestHandler):
     def get(self, tail = ''):
         lista = []
-        whitespace = re.compile('^\s+$')
-        multispace = re.compile(' +','M')
-        self.response.headers['Content-Type'] = 'text/css'
+        final = ''
+        procesado = ''
+        multispace = re.compile(' +', re.M)
+        separadores = re.compile('\s*([;:{}])\s*', re.M)
+        comentarios = re.compile('\/\*.*?\*\/', re.M | re.S)
+        self.response.headers['Content-Type'] = 'text/css; charset=utf-8'
         if tail[-4:] == '.css':
             lista = tail[:-4].split('%2C')
             for hoja in lista:
                 path = os.path.join(os.path.dirname(__file__), 'czz/' + hoja + '.css')
-#                path = os.path.join(os.path.dirname(__file__), 'index.html')
-                self.response.out.write(path)
-                self.response.out.write('/*======== '+ hoja + '.css ========*/')
-#                    with open(path,'r') as original:
-#                        self.response.out.write("OK!")
-                miCSS = open(path)
-                print(miCSS)
-                for unaLinea in miCSS:
-                    if not whitespace.match(unaLinea):
-#                       Se supone que esto es para quitar lineas en blanco.
-                        self.response.out.write(unaLinea)
-                    
+                titulo = '\n\n/*======== '+ hoja + '.css ========*/\n'
+                try:
+                    with open(path, 'U') as elCSS:
+                        procesado = elCSS.read()
+                        procesado = multispace.sub(' ', procesado)
+                        procesado = separadores.sub(r'\1', procesado)
+                        procesado = comentarios.sub('', procesado)
+                        procesado = procesado.replace("\n", "")
+                        procesado = titulo + procesado
+                    final+=procesado
+                    procesado = ''
+                except IOError:
+                    final += titulo + "\n/*ERROR: No se pudo abrir el archivo*/\n"
+            
+            self.response.out.write(final)
+        else:            
+            self.response.out.write("/*ERROR EN URL*/")
                         
                     
         
--- prueba.py
+++ prueba.py
@@ -4,15 +4,9 @@ import os
 import re
 final = ''
 procesado = ''
-whitespace = re.compile('^\s+$')
 multispace = re.compile(' +', re.M)
-sep1 = re.compile('\s*;\s*', re.M)
-sep2 = re.compile('\s*:\s*', re.M)
-sep3 = re.compile('\s*{\s*', re.M)
-sep4 = re.compile('\s*}\s*', re.M)
-sep5 = re.compile('\s*,\s*', re.M)
+separadores = re.compile('\s*([;:{}])\s*', re.M)
 comentarios = re.compile('\/\*.*?\*\/', re.M | re.S)
-enters = re.compile('\n', re.M)
 if tail[-4:] == '.css':
     lista = tail[:-4].split('%2C')
     for hoja in lista:
@@ -21,11 +15,7 @@ if tail[-4:] == '.css':
         with open(path) as elCSS:
             procesado = elCSS.read()
             procesado = multispace.sub(' ', procesado)
-            procesado = sep1.sub(';', procesado)
-            procesado = sep2.sub(':', procesado)
-            procesado = sep3.sub('{', procesado)
-            procesado = sep4.sub('}', procesado)
-            procesado = sep5.sub(',', procesado)
+            procesado = separadores.sub(r'\1', procesado)
             procesado = comentarios.sub('', procesado)
             procesado = procesado.replace("\n", "")
             procesado = titulo + procesado
