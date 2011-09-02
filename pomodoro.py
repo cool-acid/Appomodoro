ace03cb49b623b28ab90f7c6e5a8acd6647434e1
Cool Acid <coolacid217@gmail.com>
2011-09-02 13:08:39 -0500

    Corregido error de acentos

--- pomodoro.py
+++ pomodoro.py
@@ -5,10 +5,10 @@ from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 import logging
 
-__fin__ = u'Se acab贸 tu pomodoro de %s minutos! Ahora t贸mate %s minutos de descanso, te lo mereces.'
+__fin__ = u'Se acabó tu pomodoro de %s minutos! Ahora tómate %s minutos de descanso, te lo mereces.'
 __end__ = u'Your %s minutes pomodoro is over! Now take a %s minute break, you deserve it.'
 
-__findescanso__ = u'Se acabo tu descanso de %s minutos! Listo para empezar otro pomodoro?'
+__findescanso__ = u'Se acabó tu descanso de %s minutos! Listo para empezar otro pomodoro?'
 __endrest__ = u'Your %s minutes break is over! Ready to start a new pomodoro?'
 
 class Pomodoro (webapp.RequestHandler):
